bd28e01b94128f0fd9c98733f0944f500f888d1c
Paul Hodges <paul.hodges@student.education.wa.edu.au>
2023-12-22 14:00:16 +0800

    Added ts libary

---
+++ libaries/ts/src/raw.ts
@@ -0,0 +1,116 @@
+class Server {
+    private url: string;
+    private auth: string;
+
+    private fetch_path: string;
+
+    constructor(url: string, auth: string) {
+        this.url = url;
+        this.auth = auth;
+    }
+
+    private add_params(path: string, params: any): string {
+        let result = path;
+        let first = true;
+        for (let key in params) {
+            if (first) {
+                result += '?';
+                first = false;
+            } else {
+                result += '&';
+            }
+            result += key + '=' + params[key];
+        }
+        return result;
+    }
+
+    public async get(path: string): Promise<string> {
+        return fetch(this.add_params(this.url + "/get", {
+            token: this.auth,
+            path: path
+        }), {
+            method: 'GET'
+        }).then((response) => {
+            // error code
+            if (response.status != 200) {
+                throw new Error(response.statusText);
+            }
+            return response.text();
+        });
+    }
+
+    public async set(path: string, content: string): Promise<string> {
+        return fetch(this.add_params(this.url + "/set", {
+            token: this.auth,
+            path: path,
+            value: content
+        }), {
+            method: 'PUT'
+        }).then((response) => {
+            // error code
+            if (response.status != 200) {
+                throw new Error(response.statusText);
+            }
+            return response.json();
+        });
+    }
+
+    public async delete(path: string): Promise<string> {
+        return fetch(this.add_params(this.url + "/delete", {
+            token: this.auth,
+            path: path
+        }), {
+            method: 'DELETE'
+        }).then((response) => {
+            // error code
+            if (response.status != 200) {
+                throw new Error(response.statusText);
+            }
+            return response.json();
+        });
+    }
+
+    public async data(): Promise<string> {
+        return fetch(this.add_params(this.url + "/data", {
+            token: this.auth
+        }), {
+            method: 'GET'
+        }).then((response) => {
+            // error code
+            if (response.status != 200) {
+                throw new Error(response.statusText);
+            }
+            return response.text();
+        });
+    }
+
+    public async save(): Promise<boolean> {
+        return fetch(this.add_params(this.url + "/save_to_file", {
+            token: this.auth
+        }), {
+            method: 'POST'
+        }).then((response) => {
+            // error code
+            if (response.status != 200) {
+                throw new Error(response.statusText);
+            }
+            return true;
+        });
+    }
+
+    public async load(): Promise<boolean> {
+        return fetch(this.add_params(this.url + "/load_from_file", {
+            token: this.auth
+        }), {
+            method: 'POST'
+        }).then((response) => {
+            // error code
+            if (response.status != 200) {
+                throw new Error(response.statusText);
+            }
+            return true;
+        });
+    }
+}
+
+export { Server };
