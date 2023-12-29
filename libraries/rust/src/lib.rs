d678404c8034e9a7e6d2f5fd84312af2899ace7f
Isaac Turner <spacey_sooty@outlook.com>
2023-12-29 13:45:16 +0800

    1.75 has been released

--- libraries/rust/src/lib.rs
+++ libraries/rust/src/lib.rs
@@ -7,6 +7,7 @@ pub struct RawServer {
     pub auth: String,
 }
 
+#[cfg(not(feature = "async"))]
 impl RawServer {
     fn complete_url(&self, path: &str, parameters: HashMap<&str, &str>) -> String {
         let mut url = format!("{}{}", self.url, path);
@@ -88,16 +89,10 @@ impl RawServer {
     }
 }
 
-/// can't use an impl block for async yet due to 1.7.5 not being released
-pub mod non_blocking {
-    use crate::RawServer;
-    use anyhow::{anyhow, Result};
-    use reqwest::{header::CONTENT_TYPE, Client};
-    use std::collections::HashMap;
-
-    /// I'd like to return a future here but I'm not 100% sure how...
+#[cfg(feature = "async")]
+impl RawServer {
     pub async fn make_request(
-        server: &RawServer,
+        &self,
         method: &str,
         path: &str,
         parameters: HashMap<&str, &str>,
@@ -120,14 +115,14 @@ pub mod non_blocking {
         Ok(response.text().await?)
     }
 
-    pub async fn get(server: &RawServer, path: &str) -> Result<String> {
+    pub async fn get(&self, path: &str) -> Result<String> {
         let mut parameters = HashMap::new();
         parameters.insert("path", path);
         parameters.insert("token", &*server.auth);
         server.make_request("GET", path, parameters)
     }
 
-    pub async fn set(server: &RawServer, path: &str, value: &str) -> Result<String> {
+    pub async fn set(&self, path: &str, value: &str) -> Result<String> {
         let mut parameters = HashMap::new();
         parameters.insert("path", path);
         parameters.insert("value", value);
@@ -135,7 +130,7 @@ pub mod non_blocking {
         server.make_request("PUT", path, parameters)
     }
 
-    pub async fn del(server: &RawServer, path: &str) -> Result<String> {
+    pub async fn del(&self, path: &str) -> Result<String> {
         let mut parameters = HashMap::new();
         parameters.insert("path", path);
         parameters.insert("token", &*server.auth);
@@ -143,20 +138,20 @@ pub mod non_blocking {
         server.make_request("DELETE", path, parameters)
     }
 
-    pub async fn data(server: &RawServer) -> Result<String> {
+    pub async fn data(&self) -> Result<String> {
         let mut parameters = HashMap::new();
         parameters.insert("token", &*server.auth);
         server.make_request("GET", "/data", parameters)
     }
 
-    pub async fn save(server: &RawServer) -> Result<String> {
+    pub async fn save(&self) -> Result<String> {
         let mut parameters = HashMap::new();
         parameters.insert("token", &*server.auth);
 
         server.make_request("POST", "/save_to_file", parameters)
     }
 
-    pub async fn load(server: &RawServer) -> Result<String> {
+    pub async fn load(&self) -> Result<String> {
         let mut parameters = HashMap::new();
         parameters.insert("token", &*server.auth);
 
